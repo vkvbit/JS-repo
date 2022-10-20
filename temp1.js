b31792522b0e6de59d4b33fd78142231e0dc8da4
SV0052 <86582943+SV0052@users.noreply.github.com>
2022-10-20 21:12:53 +0530

    Create temp1.js

---
+++ temp1.js
@@ -0,0 +1,11 @@
+<script>
+	let numbers_array = [1, 2, 3, 4, 5];
+
+	console.log("Original Array: ");
+	console.log(numbers_array);
+
+	numbers_array.reverse();
+
+	console.log("Reversed Array: ");
+	console.log(numbers_array);
+</script>

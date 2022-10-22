f711b711d85a01a5e5d1cadb74fad4c7bff1e3dd
kcpathak <100505051+kcpathak@users.noreply.github.com>
2022-10-22 22:23:39 +0530

    Update input3.js

--- input3.js
+++ input3.js
@@ -6,11 +6,14 @@
 <p>Your browser does not support iframes.</p>
 </iframe>
 
-<p>Click the button to display the height of the iframe.</p>
+<p>Click the button to dlay the height of the iframe.</p>
 
 <button onclick="myFunction()">Try it</button>
 
+<p id="deo"></p>
 <p id="demo"></p>
+<p id="demo"></p>
+<p>Click the button to display the height of the iframe.</p>
 
 <script>
 function myFunction() {
